967307ee34d6b59b38bb8dc795f4555d8a50e5a7
royalrock11 <royalrock13@gmail.com>
2023-05-11 11:37:01 -0400

    space-flight-info

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import { useEffect, useState } from 'react';
+import SpaceFlight from './SpaceFlight';
+
+function App() {
+  return(
+    <div>
+      <h1>SpaceX Flights</h1>
+      <SpaceFlight />
+    </div>
+  )
+}
+
+export default App;
